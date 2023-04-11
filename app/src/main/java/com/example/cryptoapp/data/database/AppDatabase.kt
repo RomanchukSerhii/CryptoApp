c022c8634fe5642b0c633cad731fde5cab209211
unknown <serhiiromanchuk@gmail.com>
2023-04-11 19:34:06 +0300

    refactoring layers

--- app/src/main/java/com/example/cryptoapp/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/cryptoapp/data/database/AppDatabase.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.cryptoapp.data.model.CoinPriceInfo
+import com.example.cryptoapp.data.network.model.CoinInfoDto
 
-@Database(entities = [CoinPriceInfo::class], version = 1, exportSchema = false)
+@Database(entities = [CoinInfoDbModel::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     companion object {
 
